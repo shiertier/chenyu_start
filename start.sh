47014cd2374ebf99097fc85026ef2b38eb544f7c
shiertier <90256303+shiertier@users.noreply.github.com>
2024-11-04 17:25:31 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,9 +5,9 @@ rm -rf /root/.jupyter
 jupyter lab --generate-config
 echo  "c.ServerApp.token = 'Zeyun1234'" >> /root/.jupyter/jupyter_lab_config.py
 echo "c.ServerApp.allow_origin = '*'" >> /root/.jupyter/jupyter_lab_config.py
-echo "c.MultiKernelManager.default_kernel_name = 'bash'" >> /root/.jupyter/jupyter_lab_config.py
-mkdir -p "/root/.jupyter/lab/user-settings/\@jupyterlab/translation-extension"
-echo '{"locale": "zh_CN"}' >> "/root/.jupyter/lab/user-settings/\@jupyterlab/translation-extension/plugin.jupyterlab-settings"
+echo "c.ServerApp.terminado_settings = {'shell_command' : ['/bin/bash']}" >> /root/.jupyter/jupyter_lab_config.py
+mkdir -p "/root/.jupyter/lab/user-settings/@jupyterlab/translation-extension"
+echo '{"locale": "zh_CN"}' >> "/root/.jupyter/lab/user-settings/@jupyterlab/translation-extension/plugin.jupyterlab-settings"
 nohup jupyter lab --allow-root --ip 0.0.0.0 --port 8888 --no-browser &
 
 if [ -d "/webos" ]; then
